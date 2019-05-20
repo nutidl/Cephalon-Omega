ff23449954d9c4d00c2f412a6045bda4cfb1b0a7
nutidl <13thfloorguy+github@gmail.com>
2019-05-20 15:39:39 +0700

    Deploy functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+app.get('/timestamp', (request, response) => {
+    response.send(`${Date.now()}`);
+});
+
+app.get('/timestamp-cached', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    response.send(`${Date.now()}`);
+});
+
+exports.app = functions.https.onRequest(app);
